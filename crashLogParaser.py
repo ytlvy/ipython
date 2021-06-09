fa0f7cf59d6b8cd95438a32b6b7b8cf51ef52641
gyj <yanjie.guo@kuwo.cn>
2021-06-09 19:29:31 +0800

    add crashParser

--- crashLogParaser.py
+++ crashLogParaser.py
@@ -12,6 +12,10 @@ class crashInfo:
         self.lines      = []
         self.identifiers = []
         self.count      = 0
+        self.reason     = ""
+        self.loadAdress = ""
+        self.version    = ""
+
     def identifier(self):
         content = str("-".join(self.identifiers)).encode('utf-8')
         return hashlib.md5(content).hexdigest()
@@ -39,7 +43,13 @@ class CrashLogParaser():
                 crashInfo = self.crashs[v]
                 outF.write("===================== crash num:%d" % (crashInfo.count))
                 outF.write(os.linesep)
-                
+                outF.write("version : %s" % (crashInfo.version) )
+                outF.write(os.linesep)
+                outF.write("loadAdress : %s" % (crashInfo.loadAdress) )
+                outF.write(os.linesep)
+                outF.write(crashInfo.reason)
+                outF.write(os.linesep)
+
                 for ln in crashInfo.lines:
                     outF.write(ln)
                     outF.write(os.linesep)
@@ -48,12 +58,30 @@ class CrashLogParaser():
         fileHandle.close()
 
     def parseLine(self, line):
+        info = crashInfo()
+        from_pattern = re.compile(r'SRC:([^|]+)', re.I | re.S)
+        rs = from_pattern.search(line)
+        if rs == None or len(rs.group(1).strip()) < 1:
+            return
+        info.version = rs.group(1)
+
+        reason_pattern = re.compile(r'Exception Reason([^;]*)', re.I | re.S)
+        rs = reason_pattern.search(line)
+        if rs == None or len(rs.group(1).strip()) < 1:
+            return
+        info.reason = rs.group(0)
+
+        ld_pattern = re.compile(r'LoadAddress[^;]*(0x[A-Fa-f0-9]*)', re.I | re.S)
+        rs = ld_pattern.search(line)
+        if rs == None or len(rs.group(1).strip()) < 1:
+            return
+        info.loadAdress = rs.group(1)
 
         pattern = re.compile(r'Callstack \$ \(([^\)]*)\)', re.I | re.S)
         rs = pattern.search(line)
         if rs == None or len(rs.group(1).strip()) < 1:
             return
-        info = crashInfo()
+        
         info.source = rs.group(1)
 
         # print("==============source, %s" %(info.source))
@@ -61,12 +89,14 @@ class CrashLogParaser():
         clines = info.source.split(",")
 
         for cline in clines:
-            pattern = re.compile(r'(\s?\t{0,}\d*\s+\S+\s*0x\w+\s+([^,]*))',  re.I | re.S)
+            pattern = re.compile(r'(\s?\t{0,}\d*\s+\S+\s*0x\w+\s+([^,]*))', re.I | re.S)
             results = re.search(pattern, cline)
             if results == None or len(results.group(1).strip()) < 1:
                 print("==============cline, %s" %(cline))
                 raise ValueError("行数获取失败")
-            
+
+            # print(results.group(2))     
+
             info.identifiers.append(results.group(2))
             info.lines.append(results.group(1))
             
